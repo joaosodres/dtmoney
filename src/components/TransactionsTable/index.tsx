7df40d1df122dc98689b8a3810b0657bf287714b
joaosodres <joaov1070@gmail.com>
2022-05-23 16:41:23 -0300

    criação e estilização da home page

---
+++ src/components/TransactionsTable/index.tsx
@@ -0,0 +1,34 @@
+import { Container } from "./styles";
+
+export function TransactionsTable() {
+    return (
+        <Container>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Titulo</th>
+                        <th>Valor</th>
+                        <th>Categoria</th>
+                        <th>Data</th>
+                    </tr>
+                </thead>
+
+                <tbody>
+                    <tr>
+                        <td>Desenvolvimento de site</td>
+                        <td className="deposit">R$ 12.000,00</td>
+                        <td>Desenvolvimento</td>
+                        <td>21/05/1958</td>
+                    </tr>
+                    <tr>
+                        <td>Aluguel</td>
+                        <td className="withdraw">- R$ 1.100,00</td>
+                        <td>Casa</td>
+                        <td>21/06/1958</td>
+                    </tr>
+
+                </tbody>
+            </table>
+        </Container>
+    );
+}
